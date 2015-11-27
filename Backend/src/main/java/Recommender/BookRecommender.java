09727af76d95a2320dd97a7a3258de0f1ac61a77
guzel <giselegarifullina@gmail.com>
2015-11-27 22:13:06 +0300

    Create evaluator for recommender and tests

--- Backend/src/main/java/Recommender/BookRecommender.java
+++ Backend/src/main/java/Recommender/BookRecommender.java
@@ -5,85 +5,68 @@ package Recommender;
    for Sweaty Reader project
 */
 import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
-import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
-import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
-import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.cf.taste.recommender.Recommender;
-import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 public class BookRecommender {
-    private String fileFull;
-
-    BookRecommender(String file) {
-        this.fileFull = fileFull;
-        String fileDir  = "/home/guzel/Programming/" +
-                "SweetyReader/Backend/src/main/java/data/";
-        //String fileName ="smallDataset.csv" ;
-        String fileName ="Formated_BX-Book-Ratings.csv" ;
-        this.fileFull = fileDir + fileName;
+    String fileDir  = "/home/guzel/Programming/" +
+            "SweetyReader/Backend/src/main/java/data/";
+    //String fileName ="smallDataset.csv" ;
+    String fileName ="Formated_BX-Book-Ratings.csv" ;
+    String fileFull = fileDir + fileName;
+    protected class BookRecommenderBuilder implements RecommenderBuilder {
+        public Recommender buildRecommender(DataModel model) throws TasteException{
+            ItemSimilarity similarity = new TanimotoCoefficientSimilarity(model);
+            GenericItemBasedRecommender recommender = new GenericItemBasedRecommender
+                    (model, similarity);
+            Recommender cachingRecommender = new CachingRecommender(recommender);
+            return cachingRecommender;
+        }
     }
-
-    public List<RecommendedItem> getRecommendations(int id, int amt) throws TasteException, IOException {
+    protected DataModel getModel() throws IOException {
         //userID,itemID,value
         File file = new File(fileFull);
-
         DataModel model = new FileDataModel(file);
-        ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);
-        GenericItemBasedRecommender recommender = new GenericItemBasedRecommender
-                (model, similarity);
-        // get the recommendations for the user
-        Recommender cachingRecommender = new CachingRecommender(recommender);
+        return model;
+    }
+    private Recommender getRecommender () throws IOException, TasteException {
+        BookRecommenderBuilder brb =  new BookRecommenderBuilder();
+        return brb.buildRecommender(getModel());
+    }
+    public List<RecommendedItem> getRecommendations(int id, int amt)
+            throws TasteException, IOException {
+        Recommender cachingRecommender = getRecommender();
         List<RecommendedItem> recommendations = cachingRecommender.
                 recommend(id, amt);
         return recommendations;
     }
-
-    //to check that recommender make recommendations
-    // (for users who have enough data)
-    private void checkWork (DataModel model, ItemSimilarity similarity){
-        int amt = 5; // of recommendations
-        GenericItemBasedRecommender recommender = new GenericItemBasedRecommender
-                (model, similarity);
-        try {
-            for (LongPrimitiveIterator it = model.getUserIDs(); it.hasNext();){
-                long userId = it.nextLong();
-                List<RecommendedItem> recommendations =
-                        recommender.recommend(userId, amt);
-                if (recommendations.size() != 0){
-                    System.out.print("User ");
-                    System.out.print(userId);
-                    return;
-                }
-            }
-        } catch (TasteException e) {
-            e.printStackTrace();
-        }
-
+    public double evaluate(double trainingPercentage, double evaluationPercentage) throws TasteException, IOException {
+        DataModel model = getModel();
+        RecommenderEvaluator evaluator =
+                new AverageAbsoluteDifferenceRecommenderEvaluator();
+        RecommenderBuilder builder = new BookRecommenderBuilder();
+        double result = evaluator.evaluate(builder, null, model,
+                trainingPercentage, evaluationPercentage);
+        return result;
     }
     public static void main(String[] args) {
-        BookRecommender recommender = new BookRecommender("");
+        BookRecommender br = new BookRecommender();
         try {
-            List<RecommendedItem> recommendations = recommender.
-                    getRecommendations(8, 5);
-            System.out.println (recommendations.size());
-            for (RecommendedItem recommendation : recommendations) {
-                System.out.println(recommendation.getItemID());
-            }
+            System.out.println(br.evaluate(0.2,0.1));
         } catch (TasteException e) {
             e.printStackTrace();
         } catch (IOException e) {
---
+++ Backend/src/test/java/Recommender/RecommenderTests.java
@@ -0,0 +1,57 @@
+package Recommender;
+
+/* Item based recommender tests
+   made by Guzel Garifullina
+   for Sweaty Reader project
+*/
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.junit.Test;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class RecommenderTests extends BookRecommender {
+    //to check that recommender make recommendations
+    // (for users who have enough data)
+    private long checkWork () throws IOException, TasteException {
+        int amt = 5; // of recommendations
+        BookRecommenderBuilder brb =  new BookRecommenderBuilder();
+        DataModel model = getModel();
+        Recommender recommender = brb.buildRecommender(model);
+        for (LongPrimitiveIterator it = model.getUserIDs(); it.hasNext();){
+                long userId = it.nextLong();
+                List<RecommendedItem> recommendations =
+                        recommender.recommend(userId, amt);
+                if (recommendations.size() != 0){
+                    return userId;
+                }
+        }
+        return 0;
+    }
+    @Test
+    public void recommendForSomeoneTest() throws IOException, TasteException {
+        assertEquals(8, checkWork());
+    }
+    @Test
+    public void recommendUser() {
+        BookRecommender recommender = new BookRecommender();
+        try {
+            List<RecommendedItem> recommendations = recommender.
+                    getRecommendations(8, 5);
+            assertEquals(5, recommendations.size());
+            assertEquals(786881852, recommendations.get(0).getItemID());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (TasteException e1) {
+            e1.printStackTrace();
+        }
+    }
+}
